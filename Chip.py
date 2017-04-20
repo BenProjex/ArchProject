8122f40b30305878cbe2f9dfb39c93361cc40eec
tgg12321 <tgg12321@gmail.com>
2017-04-20 01:28:49 -0500

    Added AND Chip

--- Chip.py
+++ Chip.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
+import Wire
 
 class Chip:
 
@@ -14,3 +15,25 @@ class Chip:
         # Name property will be provided by the inheriting class
         # This will mostly be used for debugging
         pass
+
+
+class ANDChip(Chip):
+    in_wire_1= Wire()
+    in_wire_2= Wire()
+    chip_id= ""
+    out_wire= Wire()
+
+    def __init__(self, wire_1, wire_2, chip_id):
+        self.in_wire_1=wire_1
+        self.in_wire_2=wire_2
+        self.chip_id
+        self.out_wire= Wire(chip_id+"_out_wire")
+
+
+    def Action(self):
+        val_1 = self.in_wire_1.get_value()
+        val_2 = self.in_wire_2.get_value()
+        self.out_wire.set_value(val_1 & val_2)
+
+    def name(self):
+        print("Chip ID: "+self.chip_id)
