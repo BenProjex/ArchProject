0708bcb72f95568611ed209bb983b07470116816
Ben <ben.sandoval31@gmail.com>
2017-04-19 19:43:54 -0500

    Put scanner in function

--- Scanner.py
+++ Scanner.py
@@ -1,9 +1,10 @@
 # Scanner for CPU Project
 
-# Initialize variables and open files
-instList = []
-inFile = ""
-fin = fopen(infile, "r")
-for line in fin:
-   strTokens = line.split()
-   print(strTokens[1], strTokens[2])
+def scanner:
+   # Initialize variables and open files
+   instList = []
+   inFile = ""
+   fin = fopen(infile, "r")
+   for line in fin:
+      strTokens = line.split()
+      print(strTokens[1], strTokens[2])
