fae4e20b4de4a3ac87a7331296d133e61735ff90
efelan <efelan@live.com>
2017-04-20 01:29:17 -0500

    Added XOR chip

--- Chip.py
+++ Chip.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
+import Wire
 
 class Chip:
 
@@ -14,3 +15,23 @@ class Chip:
         # Name property will be provided by the inheriting class
         # This will mostly be used for debugging
         pass
+
+
+class XORChip(Chip):
+
+    wire_1 = Wire()
+    wire_2 = Wire()
+    chip_id = ""
+    output_wire = Wire()
+
+    def __init__(self, wire_1, wire_2, chip_id):
+        self.wire_1 = wire_1
+        self.wire_2 = wire_2
+        self.chip_id = chip_id
+        output_wire = Wire(self.chip_id + "_OUT_WIRE")
+
+    def Action(self):
+        self.output_wire.set_value((self.wire_1.get_value() ^ self.wire_2.get_value()))
+
+    def name(self):
+        print(self.chip_id)
