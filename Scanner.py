0bb70e6aa7364e582673d9d69cf73efe67684cda
Ben <ben.sandoval31@gmail.com>
2017-04-26 15:51:34 -0500

    Added writing to output file

--- Scanner.py
+++ Scanner.py
@@ -2,12 +2,15 @@
 
 class Scanner:
    # Initialize variables and open files
-   def __init__(self, fileName):
+   def __init__(self, fileName, fOutName):
       self.instList = []
       self.inFile = fileName
       self.fin = open(self.inFile, "r")
+      self.outFile = fOutName
+      self.fout = open(self.outFile, "w")
 
    def fetchInstructions(self):
       for line in self.fin:
          hexInstructions = line.split()
+         self.out.write(hexInstructions)
       self.fin.close()
