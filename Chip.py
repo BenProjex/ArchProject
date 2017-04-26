22150588e9327764be72c967df117840dcb1bcc6
tgg12321 <tgg12321@gmail.com>
2017-04-26 18:44:23 -0500

    Demux finished
    Demux has some assumptions about its input, hopefully it isnt an issue
    when constructing the system

--- Chip.py
+++ Chip.py
@@ -209,20 +209,97 @@ class MUX8to1Chip(Chip):
         print(self.chip_id)
 
 ###############################################################
-#
+#DEMUX2to4 Chip accepts 2 input wires, 2 enable wires, 2 Select Wires, and has 4 output wires that correspond to Registers 0-4
 ###############################################################
 
 
 class DEMUX2to4Chip(Chip):
 
-    def __init__(self, wire_1, wire_2, chip_id):
+    def __init__(self, wire_1, wire_2, enable_wire_1, enable_wire_2, select_wire_1, select_wire_2, chip_id):
         self.wire_1 = wire_1
         self.wire_2 = wire_2
-        self.output_wire = Wire.Wire(self.chip_id + "_OUT_WIRE")
+        self.enable_wire_1 = enable_wire_1
+        self.enable_wire_2 = enable_wire_2
+        self.select_wire_1 = select_wire_1  #Select wires should be a number from 0 to 3, representing the 4 register options
+        self.select_wire_2 = select_wire_2
+        self.output_wire_0 = Wire.Wire(self.chip_id + "_OUT_WIRE_0")
+        self.output_wire_1 = Wire.Wire(self.chip_id + "_OUT_WIRE_1")
+        self.output_wire_2 = Wire.Wire(self.chip_id + "_OUT_WIRE_2")
+        self.output_wire_3 = Wire.Wire(self.chip_id + "_OUT_WIRE_3")
         self.chip_id = chip_id
 
+#DEMUX2to4's action determines which input to read from based on the enable wires, then send the input to the corresponding
+#register.
+#Example: A demux with enable_wire_1 ON and enable_wire_2 ON will read from selects 1 and 2. Selects 1 and 2 should be selecting different
+#registers otherwise it will error out. Assuming Selects 1 and 2 are different registers between 0 and 3, inputs 1 and 2 are passed to the
+#respective registers
     def Action(self):
-        self.output_wire.set_value()
+        if self.enable_wire_1.get_value() == 0 and self.enable_wire_2.get_value() == 0 :
+            print("No Demux Action, both enable wires disabled")
+
+        #If only enable wire 1 is enabled, pass the values to register 0-3 based on the select wire 1
+        elif self.enable_wire_1.get_value() == 1 and self.enable_wire_2.get_value() == 0 :
+            select= self.select_wire_1.get_value()
+            if select == 0 :
+                self.output_wire_0.set_value(self.wire_1.get_value())
+            elif select == 1 :
+                self.output_wire_1.set_value(self.wire_1.get_value())
+            elif select == 2:
+                self.output_wire_2.set_value(self.wire_1.get_value())
+            elif select == 3:
+                self.output_wire_3.set_value(self.wire_1.get_value())
+            else:
+                print("Error, select wire must hold a value between 0 and 3")
+                return
+        # I dont believe there should be a situation where sel_1 is off and sel_2 is on, but just in case
+        #it will be supported, needs to be discussed.
+        elif self.enable_wire_1.get_value() == 0 and self.enable_wire_2.get_value() == 1:
+            select = self.select_wire_1.get_value()
+            if select == 0:
+                self.output_wire_0.set_value(self.wire_2.get_value())
+            elif select == 1:
+                self.output_wire_1.set_value(self.wire_2.get_value())
+            elif select == 2:
+                self.output_wire_2.set_value(self.wire_2.get_value())
+            elif select == 3:
+                self.output_wire_3.set_value(self.wire_2.get_value())
+            else:
+                print("Error, select wire must hold a value between 0 and 3")
+                return
+        #
+        elif self.enable_wire_1.get_value() == 1 and self.enable_wire_2.get_value() == 1:
+            select_1 = self.select_wire_1.get_value()
+            select_2 = self.select_wire_2.get_value()
+            if select_1 == select_2:
+                print("Error in DEMUX, both select wires have selected the same register")
+                return
+
+            #Pass input 1 to the register chosen by select 1
+            if select_1 == 0:
+                self.output_wire_0.set_value(self.wire_1.get_value())
+            elif select_1 == 1:
+                self.output_wire_1.set_value(self.wire_1.get_value())
+            elif select_1 == 2:
+                self.output_wire_2.set_value(self.wire_1.get_value())
+            elif select_1== 3:
+                self.output_wire_3.set_value(self.wire_1.get_value())
+            else:
+                print("Error, select wire must hold a value between 0 and 3")
+                return
+
+            # Pass input 2 to the register chosen by select 2
+            if select_2 == 0:
+                self.output_wire_0.set_value(self.wire_2.get_value())
+            elif select_2 == 1:
+                self.output_wire_1.set_value(self.wire_2.get_value())
+            elif select_2 == 2:
+                self.output_wire_2.set_value(self.wire_2.get_value())
+            elif select_2 == 3:
+                self.output_wire_3.set_value(self.wire_2.get_value())
+            else:
+                print("Error, select wire must hold a value between 0 and 3")
+                return
+
 
     def name(self):
         print(self.chip_id)
