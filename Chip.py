a9011283f0311e221ef7a94758e1623e49e5e9c4
tgg12321 <tgg12321@gmail.com>
2017-04-26 17:14:39 -0500

    Fixing Merge Conflicts

--- Chip.py
+++ Chip.py
@@ -144,24 +144,121 @@ class ORChip(Chip):
 ###############################################################
 
 
-class MUXChip(Chip):
+class MUX2to1Chip(Chip):
 
     wire_1 = Wire()
     wire_2 = Wire()
+    select = 0
     chip_id = ""
     output_wire = Wire()
 
     def __init__(self, wire_1, wire_2, chip_id):
+    def __init__(self, wire_1, wire_2, select_wire_1, chip_id):
         self.wire_1 = wire_1
         self.wire_2 = wire_2
+        self.select = select_wire_1.get_value()
         self.chip_id = chip_id
         self.output_wire = Wire(self.chip_id + "_OUT_WIRE")
 
     def Action(self):
         self.output_wire.set_value()
+        if self.select == 0:
+            self.output_wire.set_value(self.wire_1.get_value())
+        else:
+            self.output_wire.set_value(self.wire_2.get_value())
 
     def name(self):
         print(self.chip_id)
+
+###############################################################
+#
+###############################################################
+
+
+class MUX4to1Chip(Chip):
+
+    wire_1 = Wire()
+    wire_2 = Wire()
+    wire_3 = Wire()
+    wire_4 = Wire()
+    select = 0
+    chip_id = ""
+    output_wire = Wire()
+
+    def __init__(self, wire_1, wire_2, wire_3, wire_4, select_wire_1, chip_id):
+        self.wire_1 = wire_1
+        self.wire_2 = wire_2
+        self.wire_3 = wire_3
+        self.wire_4 = wire_4
+        self.select = select_wire_1.get_value()
+        self.chip_id = chip_id
+        self.output_wire = Wire(self.chip_id + "_OUT_WIRE")
+
+    def Action(self):
+        if self.select == 0:
+            self.output_wire.set_value(self.wire_1.get_value())
+        elif self.select == 1:
+            self.output_wire.set_value(self.wire_2.get_value())
+        elif self.select == 2:
+            self.output_wire.set_value(self.wire_3.get_value())
+        else:
+            self.output_wire.set_value(self.wire_4.get_value())
+
+    def name(self):
+        print(self.chip_id)
+
+###############################################################
+#
+###############################################################
+
+
+class MUX8to1Chip(Chip):
+    wire_1 = Wire()
+    wire_2 = Wire()
+    wire_3 = Wire()
+    wire_4 = Wire()
+    wire_5 = Wire()
+    wire_6 = Wire()
+    wire_7 = Wire()
+    wire_8 = Wire()
+    select = 0
+    chip_id = ""
+    output_wire = Wire()
+
+    def __init__(self, wire_1, wire_2, wire_3, wire_4, wire_5, wire_6, wire_7, wire_8, select_wire_1, chip_id):
+        self.wire_1 = wire_1
+        self.wire_2 = wire_2
+        self.wire_3 = wire_3
+        self.wire_4 = wire_4
+        self.wire_5 = wire_5
+        self.wire_6 = wire_6
+        self.wire_7 = wire_7
+        self.wire_8 = wire_8
+        self.select = select_wire_1.get_value()
+        self.chip_id = chip_id
+        self.output_wire = Wire(self.chip_id + "_OUT_WIRE")
+
+    def Action(self):
+        if self.select == 0:
+            self.output_wire.set_value(self.wire_1.get_value())
+        elif self.select == 1:
+            self.output_wire.set_value(self.wire_2.get_value())
+        elif self.select == 2:
+            self.output_wire.set_value(self.wire_3.get_value())
+        elif self.select == 3:
+            self.output_wire.set_value(self.wire_4.get_value())
+        elif self.select == 4:
+            self.output_wire.set_value(self.wire_5.get_value())
+        elif self.select == 5:
+            self.output_wire.set_value(self.wire_6.get_value())
+        elif self.select == 6:
+            self.output_wire.set_value(self.wire_7.get_value())
+        else:
+            self.output_wire.set_value(self.wire_8.get_value())
+
+    def name(self):
+        print(self.chip_id)
+
 
 ###############################################################
 #
