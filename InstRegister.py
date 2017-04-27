16a25203ce484a43a22ffaf3ff2d446480eb7b48
Ben <ben.sandoval31@gmail.com>
2017-04-27 00:58:39 -0500

    added to IR

--- InstRegister.py
+++ InstRegister.py
@@ -1,11 +1,18 @@
 from Chip import *
 from Wire import *
 
+import sys
+
 class InstRegister():
    
    def __init__(self):
       self.cursor = 0
       self.wire = Wire()
    
-   def action():
-      
+   def action(self, eprom):
+      if cursor >= 2**12:
+         print("Reached end of EPROM")
+         return 0
+      else:
+         currentByte = eprom.data[cursor]
+         #left off here
